4cbbce0e0716e00b57782915561ef82c995dbf94
madussree-ravi <madussree2310250@ssn.edu.in>
2025-07-17 15:25:10 +0530

    home page added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
 import Dashboard from "./pages/Dashboard";
@@ -10,6 +11,7 @@ import AddExpense from "./pages/AddExpense";
 function App() {
   return (
     <Routes>
+      <Route path="/" element={<HomePage/>}/>
       <Route path="/login" element={<LoginPage />} />
       <Route path="/register" element={<RegisterPage />} />
       <Route path="/dashboard" element={<Dashboard />} />
